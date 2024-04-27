52c86f007baed4a69b1cd0a1ae96f1a55616f781
HenriqueDummer <hatiehique2005@gmail.com>
2024-04-27 09:35:17 -0300

    First projetct

---
+++ React-Essentials/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+//--------Components---------
+import Header from "./Components/Header";
+import CoreConcepts from "./Components/CoreConcepts";
+import Examples from "./Components/Examples";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ React-Essentials/src/Components/CoreConcept.jsx
@@ -0,0 +1,12 @@
+const CoreConcept = (props) => {
+  console.log(props);
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+};
+
+export default CoreConcept;
