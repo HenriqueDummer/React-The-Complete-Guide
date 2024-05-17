52dcda5e0217226b0531c4388d100a9f1bd70f87
HenriqueDummer <hatiehique2005@gmail.com>
2024-05-17 09:00:33 -0300

    HTTP Request

---
+++ HTTP_Request/src/components/Error.jsx
@@ -0,0 +1,18 @@
+function Error({ title, message, onConfirm }) {
+  console.log('renfer Error')
+  return (
+    <div className="error">
+      <h2>{title}</h2>
+      <p>{message}</p>
+      {onConfirm && (
+        <div id="confirmation-actions">
+          <button onClick={onConfirm} className="button">
+            Okay
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Error
